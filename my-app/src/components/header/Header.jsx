2df0270364ecf3b90bd04477997c0a1e54ddd55d
jkim <jkim@dotdash.com>
2022-04-24 04:59:05 -0400

    Add header component

--- my-app/src/components/header/Header.jsx
+++ my-app/src/components/header/Header.jsx
@@ -1,16 +1,27 @@
 import React from 'react'
 import './header.scss'
+import CTA from './CTA'
+import HeaderSocials from './HeaderSocials'
+import Me from '../../assets/logo.png'
+
 
 const Header = () => {
     return (
-        <div>
-            Header
+        <header>
             <div className="container header__container">
                 <h5>Hello I'm </h5>
                 <h1>John Kim</h1>
                 <h5 className="text-light"> Fullstack Developer</h5>
+                <CTA />
+                <HeaderSocials />
+
+                <div className="me">
+                    <img src={Me} alt="me" />
+                </div>
+
+                <a href="#contact" className='scroll__down'>Scroll down</a>
             </div>
-        </div>
+        </header>
     )
 }
 
