116fc23fa21fe3c8efe80e7f292829b983f4ab6b
jkim <jkim@dotdash.com>
2022-05-21 15:29:53 -0400

    Update css of the header & Implement Experience components

--- my-app/src/components/header/Header.jsx
+++ my-app/src/components/header/Header.jsx
@@ -3,6 +3,7 @@ import './header.scss'
 import CTA from './CTA'
 import HeaderSocials from './HeaderSocials'
 import Me from '../../assets/logo.png'
+import Background from '../../assets/logo.png'
 
 import { AppWrap } from '../../wrapper/';
 
@@ -12,6 +13,7 @@ const Header = () => {
                 <h5>Hello I'm </h5>
                 <h1>John Kim</h1>
                 <h5 className="text-light"> Fullstack Developer</h5>
+                <hr/>
                 {/* 
                 <CTA />
                 <HeaderSocials />
