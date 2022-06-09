92e48fdf5fb13ab22bbe4a2c88e969e224814cac
jkim <jkim@dotdash.com>
2022-06-09 00:15:39 -0400

    Build a skeleton code for about page

--- my-app/src/components/about/About.jsx
+++ my-app/src/components/about/About.jsx
@@ -1,12 +1,37 @@
 import React from 'react'
+import { AppWrap } from "../../wrapper";
 import './about.scss'
 
 const About = () => {
     return (
         <div>
-            <section id='about'>About</section>
+            <section id='about'>
+            <div className="container about__container">
+                <div className="header">
+                    <div className="header__title">
+                        Hello. Welcome to my portfolio. 
+                    </div>
+                </div>
+                <div className="content">
+                    <div className="content__info">
+                        name
+                    </div>
+                    <div className="content__info">
+                        education
+                    </div>
+                    <div className="content__info">
+                        email
+                    </div>
+                    <div className="content__info">
+                        Location
+                    </div>
+                    
+                </div>
+            </div>
+            </section>
         </div>
     )
 }
 
-export default About
+//export default About
+export default AppWrap(About, "about", "about");
