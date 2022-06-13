ba724687ef8cc63f835be2904588f22dae9cf813
jkim <jkim@dotdash.com>
2022-06-13 02:30:27 -0400

    Implement skeleton code for footer componenet & Update css according to figma

--- my-app/src/components/footer/Footer.jsx
+++ my-app/src/components/footer/Footer.jsx
@@ -1,12 +1,31 @@
 import React from 'react'
 import './footer.scss'
+import {FaFacebookF, FaGithub} from 'react-icons/fa';
+import {BsLinkedin} from 'react-icons/bs'
 
 const Footer = () => {
     return (
-        <div>
-            footer
-        </div>
+            <div className="footer__container">
+                <div className="footer__icons">
+                    <div className="footer__icons--wrapper">
+                        <div className="footer__icon">
+                            <a href="https://linkedin.com"> <FaFacebookF /></a>
+                        </div>
+                        <div className="footer__icon">
+                            <a href="https://linkedin.com" target="_blank"> <BsLinkedin /></a>
+                        </div>
+                        <div className="footer__icon">
+                            <a href="https://github.com" target="_blank"> <FaGithub /></a>
+                        </div>
+                    </div>
+                </div>
+                <div className="footer__copyright">
+                    <p className="p-text">@2022 John Kim</p>
+                    <p className="p-text">All rights reserved</p>
+                </div>
+            </div>
     )
 }
 
 export default Footer
+//export default AppWrap(Footer, 'footer', 'footer');
